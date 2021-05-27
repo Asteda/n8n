a341089b211d64366adb4ddb37f7099ddff626a7
AurelieAbraham <alexinedu18@gmail.com>
2021-05-27 16:34:35 +0200

    fusion des trois noeuds + création d'une sortie pour le noeud LdSimilarity (sortie en dur)

--- packages/nodes-base/nodes/LdSimilarity/LdSimilarity.node.ts
+++ packages/nodes-base/nodes/LdSimilarity/LdSimilarity.node.ts
@@ -33,6 +33,121 @@ export class LdSimilarity implements INodeType {
 			// Node properties which the user gets displayed and
 			// can change on the node.
 
+			{
+				displayName: 'Resource',
+				name: 'resource',
+				type: 'options',
+				options: [
+					{
+						name: 'Files',
+						value: 'files',
+					},
+					{
+						name: 'URIs',
+						value: 'uris',
+					},
+				],
+				default: 'uris',
+				required: true,
+				description: 'Resource to consume',
+			},
+
+			{
+				displayName: 'URL Resource 1',
+				name: 'url1',
+				type: 'string',
+				required: true,
+				default:'',
+				description:'URL of the first resource',
+				displayOptions: {
+					show: {
+						resource: [
+							'uris',
+						]	,
+					},
+				},
+			},
+
+			{
+				displayName: 'URL Resource 2',
+				name: 'url2',
+				type: 'string',
+				required: true,
+				default:'',
+				description:'URL of the second resource',
+				displayOptions: {
+					show: {
+						resource: [
+							'uris',
+						]	,
+					},
+				},
+			},
+
+			{
+				displayName: 'SpreadSheet ID Input',
+				name: 'sheetIdInput',
+				type: 'string',
+				required: true,
+				default:'',
+				description:'ID of the Google Sheet document to get the resources (you can get it between' +
+					'"/d/" and "/edit" in the URL file)',
+				displayOptions: {
+					show: {
+						resource: [
+							'files',
+						]	,
+					},
+				},
+			},
+
+			{
+				displayName: 'Number of threads',
+				name: 'nbThreads',
+				type: 'number',
+				required: true,
+				typeOptions: {
+					maxValue: 10,
+					minValue: 1,
+					numberStepSize: 1,
+				},
+				default: 1,
+				description: 'How many threads to calculate the similarity',
+			},
+
+			// lddata set = choix
+
+			{
+				displayName: 'DataSet Choice',
+				name: 'datasetChoice',
+				type: 'options',
+				options: [
+					{
+						name: 'DBPedia_en',
+						value: 'DBPedia_en',
+					},
+					{
+						name: 'DBPedia_fr',
+						value: 'DBPedia_fr',
+					},
+					{
+						name: 'DBPedia_it',
+						value: 'DBPedia_it',
+					},
+				],
+				default: 'DBPedia_en', // The initially selected option
+				description: 'The main dataset object for querying data',
+			},
+
+			// useindex bool
+
+			{
+				displayName: 'Use index',
+				name: 'useIndex',
+				type: 'boolean',
+				default: true, // Initial state of the toggle
+				description: 'Specifies wether true or false for index usage',
+			},
 
 			{
 				displayName: 'Type of measure',
@@ -105,13 +220,52 @@ export class LdSimilarity implements INodeType {
 				description: 'Type of measure',
 			},
 
-
+			{
+				displayName: 'SpreadSheet ID Output',
+				name: 'sheetIdOutput',
+				type: 'string',
+				required: true,
+				default:'',
+				description:'ID of the Google Sheet document to write the result (you can get it between' +
+					'"/d/" and "/edit" in the URL file) <br/> This file MUST have 3 columns : resource1, resource2, result.',
+			},
 
 
 		],
 	};
 
 	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
-		return [[]];
+		//let responseData;
+
+		const measureType = this.getNodeParameter('measureType', 0) as string;
+		const numberOfThreads = this.getNodeParameter('nbThreads', 0) as number;
+
+		/*const options: OptionsWithUri = {
+			headers: {
+				'Accept': 'application/json',
+				//'Authorization': `Bearer ${credentials.apiKey}`,
+			},
+			method: 'PUT',
+			body: {
+
+			},
+			uri: `https://api.sendgrid.com/v3/marketing/contacts`,
+			json: true,
+		};*/
+
+		//responseData = await this.helpers.request(options);
+
+
+
+		//return [this.helpers.returnJsonArray(responseData)];
+
+		return [this.helpers.returnJsonArray({
+			resource1: measureType,
+			resource2: numberOfThreads,
+			result : '0.66442',
+		})];
+
+
+
 	}
 }
